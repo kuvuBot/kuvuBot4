4c9f33a695f1b1aaa68e2626e8efbfee6bbb8cce
Jqmey <jqmey@outlook.com>
2018-02-16 16:30:33 +0100

    Dodanie komendy .figlet

---
+++ commands/figlet.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const figlet = require('figlet');
+const util = require('util');
+
+const makeFiglet = util.promisify(figlet.text);
+
+exports.info = {
+    command: '.figlet',
+    help: {
+        command: '.figlet <tekst>',
+        description: 'generuje figlet'
+    },
+    parameters: [
+        'args',
+        'message'
+    ]
+};
+
+exports.function = async (parameters) => {
+    const args = parameters.args;
+    const message = parameters.message;
+
+    const text = args.slice(1).join(' ');
+
+    if(!text) {
+        message.reply('prawidłowe użycie: `.figlet <tekst>`!');
+    } else {
+        if(text.length > 16) {
+            message.reply('tekst nie może mieć więcej niż 16 znaków!');
+        } else {
+            const figletText = await makeFiglet(text);
+            message.reply('\n\n```\n' + figletText + '\n```');
+        }
+    }
+};
