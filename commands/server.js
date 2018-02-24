abcf7e7c3e98a1c9298d8eb85f8f0eae405b36bf
Jqmey <jqmey@outlook.com>
2018-02-24 19:09:23 +0100

    Dodanie komendy .serwer

---
+++ commands/server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Discord = require('discord.js');
+const moment = require('moment');
+
+exports.info = {
+    command: '.serwer',
+    help: {
+        command: '.serwer',
+        description: 'zwraca informacje o serwerze'
+    },
+    parameters: [
+        'config',
+        'message'
+    ]
+};
+
+exports.function = async (parameters) => {
+    const config = parameters.config;
+    const message = parameters.message;
+
+    const guild = message.guild;
+
+    if(!guild) {
+        message.reply('tą komendę można używać jedynie na serwerze!');
+    } else {
+        const embed = new Discord.RichEmbed();
+        embed.setAuthor('Informacje o serwerze', message.client.user.displayAvatarURL);
+        embed.setColor(config.colors.default);
+
+        if(guild.iconURL) {
+            embed.setThumbnail(guild.iconURL);
+        }
+
+        embed.addField('Nazwa', guild.name, true);
+        embed.addField('Właściciel', guild.owner.displayName, true);
+        embed.addField('Ilość członków', guild.members.size, true);
+        embed.addField('Ilość ról', guild.roles.size, true);
+        embed.addField('Region', guild.region.toUpperCase(), true);
+        embed.addField('Czas stworzenia', moment(guild.createdAt).format('DD.MM.YYYY, HH:mm'), true);
+
+        message.channel.send(embed);
+    }
+};
