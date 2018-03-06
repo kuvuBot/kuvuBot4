7ec102cd3db81880f0017149891b81b5bc90e05c
kuvuś <kuba17ism@tuta.io>
2018-03-06 21:41:30 +0100

    Dodanie komendy .reaguj

---
+++ commands/react.js
@@ -0,0 +1,33 @@
+'use strict';
+
+exports.info = {
+    command: '.reaguj',
+    help: {
+        command: '.reaguj <tekst>',
+        description: 'dodaje reakcje do wiadomości'
+    },
+    parameters: [
+        'args',
+        'message'
+    ]
+};
+
+exports.function = async (parameters) => {
+    const args = parameters.args;
+    const message = parameters.message;
+
+    const chars = args.slice(1).join(' ').toLowerCase().split('');
+    const regA = 127462;
+
+    if(!chars[0]) {
+        message.reply('prawidłowe użycie: `.reaguj <tekst>`!');
+    } else {
+        for(const char in chars) {
+            if(chars[char].charCodeAt() > 96 && chars[char].charCodeAt() < 123) {
+                let emojiCode = regA + chars[char].charCodeAt() - 97;
+                let emoji = String.fromCodePoint(emojiCode);
+                await message.react(emoji);
+            }
+        }
+    }
+};
