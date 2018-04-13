5b177dd50129aa7e9d24b8847a369e1653ac8394
kuvuś <kuba17ism@tuta.io>
2018-04-13 21:28:22 +0200

    Dodanie .kpn

---
+++ commands/rps.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const Discord = require('discord.js');
+
+exports.info = {
+    command: '.kpn',
+    help: {
+        command: '.kpn <kamień/papier/nożyce>',
+        description: 'kamień, papier, nożyce z BOTem'
+    },
+    parameters: [
+        'args',
+        'config',
+        'message'
+    ]
+};
+
+exports.function = async (parameters) => {
+    const args = parameters.args;
+    const config = parameters.config;
+    const message = parameters.message;
+
+    let thing = args[1].toLowerCase();
+    const emojis = {
+        kamień: '⚪ Kamień',
+        papier: '📰 Papier',
+        nożyce: '✂ Nożyce',
+    };
+    const words = ['⚪ Kamień', '📰 Papier', '✂ Nożyce'];;
+
+    if (!args[1].toLowerCase() == "kamien" || !args[1].toLowerCase() == "kamień" || !args[1].toLowerCase() == "nożyce" || !args[1].toLowerCase() == "nozyce" || !args[1].toLowerCase() == "papier") {
+        await message.reply('prawidłowe użycie: `.kpn <kamień/papier/nożyce>`!');
+    } else {
+        thing = thing.replace(/kamień|papier|nożyce/gi, function(matched){
+            matched = matched.replace(/\s/g, '_');
+            return emojis[matched];
+        });
+
+        const botThing = words[Math.floor(Math.random() * 3)];
+
+        const compare = function(thing, botThing) {
+            if (thing === botThing) {
+                return '🏳 Remis!';
+            } else if (thing === emojis['kamień']) {
+                if (botThing === emojis['nożyce']) {
+                    return '🎉 Wygrałeś!';
+                }
+            } else if (thing === emojis['papier']) {
+                if (botThing === emojis['kamień']) {
+                    return '🎉 Wygrałeś!';
+                }
+            } else if (thing === emojis['nożyce']) {
+                if (botThing === emojis['kamień']) {
+                    return '🥊 Przegrałeś :c';
+
+                }
+            }
+        };
+        let result = compare(thing, botThing);
+        if (result == undefined) {
+            result = '🥊 Przegrałeś :c';
+        }
+        const embed = new Discord.RichEmbed();
+        embed.setAuthor('Papier, kamień nożyce', message.client.user.displayAvatarURL);
+        embed.setColor(config.colors.default);
+        embed.addField('Ty', thing, true);
+        embed.addField('Bot', botThing, true);
+        embed.addField('Wynik', result, true);
+
+        await message.channel.send(embed);
+     }
+
+};
