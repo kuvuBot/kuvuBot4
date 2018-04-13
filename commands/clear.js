f8bc108f4bace46046bd3215cc4994a60d741843
kuvuś <kuba17ism@tuta.io>
2018-04-13 21:28:08 +0200

    Dodanie .czyść

---
+++ commands/clear.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const Discord = require('discord.js');
+
+exports.info = {
+    command: '.czyść',
+    help: {
+        command: '.czyść <liczba wiadomości>',
+        description: 'usuwa podaną liczbę wiadomości'
+    },
+    parameters: [
+        'args',
+        'message'
+    ]
+};
+
+exports.function = async (parameters) => {
+    const args = parameters.args;
+    const message = parameters.message;
+
+    if(!message.guild) {
+        await message.reply('ta komenda jest dostępna tylko na serwerach!');
+    } else {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) {
+            await message.reply('nie masz wystarczających uprawnień, aby wykonać tą komendę!');
+        } else {
+            const messages = args[1];
+
+            if (isNaN(messages)) {
+                await message.reply('prawidłowe użycie: `.czyść <liczba wiadomości>`!');
+            } else {
+                if (messages < 2) {
+                    await message.reply('ilość wiadomości nie może być mniejsza niż 2!');
+                } else if (messages > 100) {
+                    await message.reply('ilość wiadomości nie może być większa niż 100!');
+                } else {
+                    const deletedMessages = await message.channel.bulkDelete(messages);
+                    await message.reply(`pomyślnie usunięto ${deletedMessages.size} wiadomości!`);
+                }
+            }
+        }
+    }
+};
