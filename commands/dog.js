f615821a2fd5e7832e4b23020aa92f2c8b4ba37d
kuvuś <kuba17ism@tuta.io>
2018-07-22 11:16:20 +0200

    Add dog command

---
+++ commands/dog.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const Discord = require('discord.js');
+const httpAsPromised = require('http-as-promised');
+
+exports.info = {
+    command: 'dog',
+    help: {
+        command: 'dog',
+        description: 'wysyła losowego pieska.',
+        category: 'fun'
+    },
+    aliases: [
+        'pies',
+        'piesek'
+
+    ]
+};
+
+exports.function = async (parameters) => {
+    const config = parameters.config;
+    const message = parameters.message;
+    const lang = parameters.lang;
+    const db = parameters.db;
+    const version = parameters.packageInfo.version;
+
+    const url = 'https://api.thedogapi.com/v1/images/search';
+    const dog = JSON.parse(await httpAsPromised.get(url, { resolve : 'body' }));
+
+    const embed = new Discord.RichEmbed();
+    embed.setAuthor(await db.getTrans(lang, 'dog_title'), message.client.user.displayAvatarURL);
+    embed.setColor(config.colors.default);
+    embed.setImage(dog[0].url);
+    embed.setFooter(`kuvuBot ${version}`);
+    embed.setTimestamp();
+
+    await message.channel.send(embed);
+};
