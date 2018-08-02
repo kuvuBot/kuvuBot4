9f8780692a4b3ce545a07c7fb84e84055f5e9a6d
kuvuś <kuba17ism@tuta.io>
2018-08-02 22:49:46 +0200

    Add invite command

---
+++ commands/invite.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Discord = require('discord.js');
+
+exports.info = {
+    command: 'inv',
+    help: {
+        command: 'invite',
+        description: 'generates channel invite',
+        category: 'gen'
+    },
+    aliases: [
+        'zaproszenie',
+        'invite'
+    ]
+};
+
+exports.function = async (parameters) => {
+    const message = parameters.message;
+    const lang = parameters.lang;
+    const db = parameters.db;
+
+    if(!message.guild) {
+        await message.reply(await db.getTrans(lang, 'onlyText'));
+    } else {
+        let invite = await message.channel.createInvite();
+        await message.channel.send(invite.url);
+    }
+};
